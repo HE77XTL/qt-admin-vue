55df917d0024fee1a3e13fefb928c44a37783163
何东升 <heds@signalway.com.cn>
2023-04-04 15:59:21 +0800

    feat: form-item component optimization

--- src/pages/demo/components/form-item.vue
+++ src/pages/demo/components/form-item.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="formItem">
-    <el-input v-if="itemVisible('input')" v-model="value" v-bind="$attrs" class="formItemInput itemRow">
-    </el-input>
-    <el-select v-if="itemVisible('select')" v-model="value" v-bind="$attrs" class="formItemSelect itemRow">
+    <el-input v-if="itemVisible('input')" v-model="value" v-bind="$attrs" class="formItemInput itemRow" :placeholder="placeholder"></el-input>
+    <el-select v-if="itemVisible('select')" v-model="value" v-bind="$attrs" class="formItemSelect itemRow" :placeholder="placeholder">
       <el-option
           v-for="item in props.options"
           :key="item.value"
@@ -13,25 +12,33 @@
   </div>
 </template>
 <script lang="ts">
-  export default {
-    inheritAttrs: false
-  };
+  export default {inheritAttrs: false};
 </script>
 <script setup lang="ts">
-  const props = defineProps(['type', 'modelValue', 'options']);
+  interface Props {
+    itemType: "input" | "select"
+    modelValue?: string | number
+    options?: Array<{ label: string, value: string | number }>
+    placeholder?: string
+  }
+
+  const props = defineProps<Props>()
   const emit = defineEmits(['input', 'update:modelValue']);
   const value = ref(props.modelValue);
+  const itemVisible = (itemType: string): boolean => props.itemType === itemType;
+  const placeholder = computed(() => {
+    if (props.placeholder) {
+      return props.placeholder;
+    }
+    const placeholderMap = {
+      input: '请输入',
+      select: '请选择'
+    }
+    return placeholderMap[props.itemType] || '';
+  })
 
-  const itemVisible = (type: string): boolean => type === props.type;
-
-  watch(
-      () => props.modelValue,
-      (val: string) => {
-        value.value = val;
-      }
-  );
-
-  watch(value, (val: string) => emit("update:modelValue", val));
+  watch(() => props.modelValue,(val) => value.value = val);
+  watch(value, (val) => emit("update:modelValue", val));
 
 
 </script>
@@ -39,7 +46,7 @@
 <style scoped lang="scss">
   .formItem {
     .itemRow {
-      width: 280px;
+      width: var(--input-width);
     }
   }
 </style>
--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -2,18 +2,19 @@
   <div class="demoPage">
     <div>{{aValue}}</div>
     <div>{{bValue}}</div>
-    <FormItem type="input" v-model="aValue" class="parentDefineClassTets"></FormItem>
-    <FormItem type="select" v-model="bValue" :options="bOptions" class="parentDefineClassTets"></FormItem>
+    <FormItem itemType="input" v-model="aValue" class="parentDefineClassTets"></FormItem>
+    <FormItem itemType="select" v-model="bValue" :options="bOptions" class="parentDefineClassTets"></FormItem>
   </div>
 </template>
 <script setup lang="ts">
+  import FormItem from './components/form-item.vue';
+
   interface demoFormInterface {
     name: string
     gender?: string
     married?: boolean
   }
 
-  import FormItem from './components/form-item.vue';
 
   const aValue = ref('defaultInput');
   const bValue = ref('apple');
